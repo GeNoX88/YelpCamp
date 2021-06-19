bfe543e9ac987880c6111a10f9801f3a6247f3df
恩欽 <fm06184cj3@gmail.com>
2021-06-19 21:34:31 +0800

    make it better

--- app.js
+++ app.js
@@ -89,11 +89,11 @@ const scriptSrcUrls = [
 ];
 const styleSrcUrls = [
     "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com/",
     "https://api.mapbox.com/",
     "https://api.tiles.mapbox.com/",
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net"
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
